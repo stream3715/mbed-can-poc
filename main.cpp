e2c87ace29ce18a59689f919b75d67fa8cb4de03
strea <stream.larn3@gmail.com>
2023-09-03 12:51:37 +0000

    CAN2を削除

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
-#include "mbed.h"
-
 #include "CAN.h"
-#include "NoMutexCAN.h"
+#include "mbed.h"
 
 #include "constants.h"
 
-NoMutexCAN can1(CAN1_RD, CAN1_TD); // CAN (PinName rd, PinName td);
-NoMutexCAN can2(CAN2_RD, CAN2_TD); // CAN (PinName rd, PinName td);
+CAN can1(CAN1_RD, CAN1_TD); // CAN (PinName rd, PinName td);
+// CAN can2(CAN2_RD, CAN2_TD); // CAN (PinName rd, PinName td);
 
 char counter = 0;
 
 Ticker ticker;
+Thread canReadThread;
+
+EventQueue queue(32 * EVENTS_EVENT_SIZE);
 
 unsigned int filterMask = 0x00000001;
 unsigned int filter1Id = 0x00000001;
-unsigned int filter2Id = 0x00000000;
+// unsigned int filter2Id = 0x00000000;
 
-int sendAll(CAN *can) {
+int sendAll() {
   int returnVal = 1;
-  for (int i = 0; i < 8; i++) {
-    int result = (*can).write(CANMessage(i, &counter, 1));
-    printf("Message sent: %d\n", counter);
+  for (int i = 1; i <= 8; i++) {
+    int result = can1.write(CANMessage(i, &counter, 1));
+    printf("%p Message sent: %d\n", &can1, counter++);
+    if(result != 1) {
+        printf("Failed to send data.");
+    }
     returnVal &= result;
   }
 
@@ -28,49 +32,58 @@ int sendAll(CAN *can) {
 }
 
 void sendCANTask() {
-  sendAll(&can1);
-  ThisThread::sleep_for(10ms);
-
-  sendAll(&can2);
-  ThisThread::sleep_for(10ms);
+  sendAll();
+  //   sendAll(&can2);
 }
 
 void readCAN1() {
+  printf("reading CAN1...\n");
   CANMessage msg;
   if (can1.read(msg)) {
-    printf("Message received: %d\n", msg.data[0]);
+    printf("CAN1 Message received: %d\n", msg.data[0]);
   }
 }
 
-void readCAN2() {
-  CANMessage msg;
-  if (can2.read(msg)) {
-    printf("Message received: %d\n", msg.data[0]);
-  }
-}
+// void readCAN2() {
+//   printf("reading CAN2...\n");
+//   CANMessage msg;
+//   if (can2.read(msg)) {
+//     printf("CAN2 Message received: %d\n", msg.data[0]);
+//   }
+// }
 
 void setUpFilter() {
-  can1.filter(filter1Id, filterMask, CANAny, 1);
-  can2.filter(filter2Id, filterMask, CANAny, 14);
+  can1.filter(filter1Id, filterMask, CANStandard, 1);
+  //   can2.filter(filter2Id, filterMask, CANStandard, 14);
 }
+
+void can1ISR() {
+  printf("ISR CAN1\n");
+  queue.call(readCAN1);
+}
+
+// void can2ISR() {
+//   printf("ISR CAN2\n");
+//   queue.call(readCAN2);
+// }
 
 int main() {
   printf("init start\n");
-  can1.frequency(1000000);
-  can2.frequency(1000000);
-  printf("can freq fin\n");
+  can1.frequency(1 * 1000 * 1000);
+  can1.mode(CAN::Normal);
+  //   can2.frequency(1000000);
+  //   can2.mode(CAN::Normal);
 
   setUpFilter();
   printf("can filter fin\n");
 
-  ticker.attach(&sendCANTask, 5000ms);
-  printf("ticker attach fin\n");
-  printf("init fin\n");
+  can1.attach(can1ISR, CAN::RxIrq);
+  //   can2.attach(can2ISR, CAN::RxIrq);
+  canReadThread.start(callback(&queue, &EventQueue::dispatch_forever));
+  printf("can attach fin\n");
 
   while (1) {
-    readCAN1();
-    ThisThread::sleep_for(10ms);
-    readCAN2();
-    ThisThread::sleep_for(10ms);
+    sendCANTask();
+    ThisThread::sleep_for(5s);
   }
 }
