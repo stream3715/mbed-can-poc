a742da849e99ffa940f5ca3ee1c79ce985a46a19
strea <stream.larn3@gmail.com>
2023-09-07 12:45:10 +0000

    Writeを削除した

--- main.cpp
+++ main.cpp
@@ -8,35 +8,13 @@ CAN can1(CAN1_RD, CAN1_TD); // CAN (PinName rd, PinName td);
 
 char counter = 0;
 
-Ticker ticker;
 Thread canReadThread;
-Thread canWriteThread;
 
 EventQueue queueRead(32 * EVENTS_EVENT_SIZE);
-EventQueue queueWrite(32 * EVENTS_EVENT_SIZE);
 
 unsigned int filterMask = 0x00000001;
 unsigned int filter1Id = 0x00000001;
 // unsigned int filter2Id = 0x00000000;
-
-int sendAll() {
-  int returnVal = 1;
-  for (int i = 1; i <= 8; i++) {
-    int result = can1.write(CANMessage(i, &counter, 1));
-    printf("%p Message sent: %d\n", &can1, counter++);
-    if (result != 1) {
-      printf("Failed to send data.\n");
-    }
-    returnVal &= result;
-  }
-
-  return returnVal;
-}
-
-void sendCANTask() {
-  sendAll();
-  //   sendAll(&can2);
-}
 
 void readCAN1() {
   printf("reading CAN1...\n");
@@ -63,10 +41,6 @@ void can1ISR() {
   queueRead.call(readCAN1);
 }
 
-void can1Write() {
-  queueWrite.call(sendAll);
-}
-
 // void can2ISR() {
 //   printf("ISR CAN2\n");
 //   queue.call(readCAN2);
@@ -74,7 +48,7 @@ void can1Write() {
 
 int main() {
   printf("init start\n");
-  can1.frequency(5 * 100 * 1000);
+  can1.frequency(1000E3);
   can1.mode(CAN::Normal);
   //   can2.frequency(1000000);
   //   can2.mode(CAN::Normal);
@@ -86,7 +60,4 @@ int main() {
   //   can2.attach(can2ISR, CAN::RxIrq);
   canReadThread.start(callback(&queueRead, &EventQueue::dispatch_forever));
   printf("can attach fin\n");
-
-  canWriteThread.start(callback(&queueWrite, &EventQueue::dispatch_forever));
-  ticker.attach(can1Write, 1s);
 }
