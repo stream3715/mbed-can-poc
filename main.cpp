e6818369906f1d79be512c342c8b49a667344730
strea <stream.larn3@gmail.com>
2023-09-07 12:43:11 +0000

    とりあえず通ったので

--- main.cpp
+++ main.cpp
@@ -4,16 +4,20 @@ CAN can1(PB_8, PB_9);
 EventQueue queue(32 * EVENTS_EVENT_SIZE);
 Thread t;
 
-int main() {
-  printf("F446\n");
-  can1.frequency(500E3);
-  t.start(callback(&queue, &EventQueue::dispatch_forever));
+void readCAN() {
+  printf("readCAN triggered\n");
 
   CANMessage msg;
-  while (1) {
-    if (can1.read(msg)) {
-      printf("Msg ID: %d, data: %d\n", msg.id, msg.data[0]);
-    }
-    thread_sleep_for(100);
+  if (can1.read(msg)) {
+    printf("Msg ID: %d, data: %d\n", msg.id, msg.data[0]);
   }
+}
+
+void readISR() { queue.call(readCAN); }
+
+int main() {
+  printf("F446\n");
+  can1.frequency(1000E3);
+  can1.attach(callback(readISR));
+  t.start(callback(&queue, &EventQueue::dispatch_forever));
 }
