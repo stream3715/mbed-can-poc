07f1ed2b9a1b5e44373bffad26ca4412657a1307
strea <stream.larn3@gmail.com>
2023-09-08 16:10:58 +0000

    ベアメタルテスト

--- main.cpp
+++ main.cpp
@@ -3,17 +3,12 @@
 
 #include "constants.h"
 
-CAN can1(CAN1_RD, CAN1_TD); // CAN (PinName rd, PinName td);
+CAN can1(CAN1_RD, CAN1_TD, 1000E3); // CAN (PinName rd, PinName td);
 // CAN can2(CAN2_RD, CAN2_TD); // CAN (PinName rd, PinName td);
 
 char counter = 0;
 
-Thread canReadThread;
-
-EventQueue queueRead(32 * EVENTS_EVENT_SIZE);
-
-unsigned int filterMask = 0x00000001;
-unsigned int filter1Id = 0x00000001;
+unsigned int filterMask = 0xFFF;
 // unsigned int filter2Id = 0x00000000;
 
 void readCAN1() {
@@ -33,12 +28,9 @@ void readCAN1() {
 // }
 
 void setUpFilter() {
-  can1.filter(filter1Id, filterMask, CANStandard, 1);
+  can1.filter(0x001, filterMask, CANStandard, 0);
+  can1.filter(0x003, filterMask, CANStandard, 0);
   //   can2.filter(filter2Id, filterMask, CANStandard, 14);
-}
-
-void can1ISR() {
-  queueRead.call(readCAN1);
 }
 
 // void can2ISR() {
@@ -48,7 +40,6 @@ void can1ISR() {
 
 int main() {
   printf("init start\n");
-  can1.frequency(1000E3);
   can1.mode(CAN::Normal);
   //   can2.frequency(1000000);
   //   can2.mode(CAN::Normal);
@@ -56,8 +47,6 @@ int main() {
   setUpFilter();
   printf("can filter fin\n");
 
-  can1.attach(can1ISR, CAN::RxIrq);
-  //   can2.attach(can2ISR, CAN::RxIrq);
-  canReadThread.start(callback(&queueRead, &EventQueue::dispatch_forever));
+  can1.attach(readCAN1, CAN::RxIrq);
   printf("can attach fin\n");
 }
